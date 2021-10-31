59b28cbd9cf54cbcd785867af047d8dc31d14c1e
[rmishra3-ro] <[rmishra3@uncc.edu]>
2021-10-31 16:12:03 -0400

    second commit

---
+++ app/src/main/java/com/example/gymbudd/TrainerSignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.gymbudd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrainerSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainer_sign_up);
+    }
+}
